d63f4697480a1f5c0a51aed5c4fe60f5313ad3be
«Natali» <feichka@yandex.ru>
2022-07-18 21:25:07 +0300

    Add task23

--- task19/Program.cs
+++ task19/Program.cs
@@ -5,7 +5,7 @@
 
 Console.WriteLine("Введите пятизначное число: ");
 string? n = Console.ReadLine();
-    if(n[0] == n[4] || n[1] == n[3])
-        Console.WriteLine("палиндром");
-    else
-        Console.WriteLine("нет");
+if(n[0] == n[4] || n[1] == n[3])
+    Console.WriteLine("палиндром");
+else
+    Console.WriteLine("нет");
