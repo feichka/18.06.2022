797e09b635455d2f45cf3ba909f04124a1791752
«Natali» <feichka@yandex.ru>
2022-07-19 00:54:19 +0300

    Changr task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,9 +3,11 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите пятизначное число: ");
+Console.WriteLine("Введите пятизначное число");
+//int n = Convert.ToInt32(Console.ReadLine());
 string? n = Console.ReadLine();
-if(n[0] == n[4] || n[1] == n[3])
+if((n[0] == n[4]) && n[1] == n[3])
     Console.WriteLine("палиндром");
 else
     Console.WriteLine("нет");
+
